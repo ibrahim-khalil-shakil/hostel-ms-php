a0c934fed02aaa96b66aaa1b4f16fceefa60ad03
ikshakil <ikssakil2@gmail.com>
2023-06-07 09:28:10 +0600

    crud done

--- database/hostel.sql
+++ database/hostel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2023 at 09:34 AM
+-- Generation Time: Jun 07, 2023 at 05:27 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
