9155d29892e995f2e5b7555f5830efa39f001d7d
Safayet99 <safayetazad99@gmail.com>
2023-06-22 12:52:22 +0600

    link updated

--- include/header.php
+++ include/header.php
@@ -4,7 +4,8 @@ if (!isset($_SESSION['userid'])) {
   echo "<script>window.location='login.php'</script>";
   exit;
 }
-$base_url = "http://localhost/hostel/";
+$protocol = isset($_SERVER['HTTPS']) ? 'https' : 'http';
+  $base_url="$protocol://".$_SERVER['SERVER_NAME']."/".explode('/',$_SERVER['SCRIPT_NAME'])[1]."/";
 require_once('class/crud.php');
 $mysqli = new crud;
 ?>
